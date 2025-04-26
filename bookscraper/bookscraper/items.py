7224ccf871d102efd2fa7d533d7964524b8b0c4e
Inbal <elmaleh.inbal@gmail.com>
2025-04-26 08:57:39 +0300

    cleaned data using pipelines

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -8,5 +8,26 @@ import scrapy
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+def serialize_price(value):
+    return f'£ {str(value)}'
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    # price_excl_tax = scrapy.Field(serializer=serialize_price)
+    # This is how you would use a serializer for small manipulations on data.
+    # Notice that anything more serious will be better done by piplines
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,67 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        # Strip all whitespaces from str
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+        
+        # Category & product type --> switch to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        # Price --> convert to float
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            
+            num = 0.0
+            if (len(value) > 0):
+                num = float(value)
+            adapter[price_key] = float(num)
+        
+        # Availability --> take num and switch to int
+        availability_str = adapter.get('availability')
+        split_avail_str = availability_str.split('(')
+
+        num = 0
+        if (len(split_avail_str)) > 1:
+            num_str = split_avail_str[1].split(' ')
+            num = int(num_str[0])
+
+        adapter['availability'] = int(num)
+
+        # Reviews --> convert str to int
+        num_reviews_str = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_str)
+
+        # Star rating --> to int
+        star_rating_str = adapter.get('stars')
+        stars_str = star_rating_str.split(' ')[1].lower()
+
+        stars = 0
+
+        if stars_str == 'one':
+            stars = 1
+        elif stars_str == 'two':
+            stars = 2
+        elif stars_str == 'three':
+            stars = 3
+        elif stars_str == 'four':
+            stars = 4
+        elif stars_str == 'five':
+            stars = 5
+        
+        adapter['stars'] = stars
+
+
+
         return item
